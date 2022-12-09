1d274ebf52b483c4883f1fcdfb9f59a2dcfbb53d
Mingwei Samuel <mingwei.samuel@gmail.com>
2022-12-09 11:27:14 -0800

    Move kvs example from `hydro-project/hydroflow`

---
+++ src/helpers.rs
@@ -0,0 +1,24 @@
+use crate::protocol::KVSMessage;
+use regex::Regex;
+
+pub fn parse_command(line: String) -> Option<KVSMessage> {
+    let re = Regex::new(r"([A-z]+)\s+(.+)").unwrap();
+    let caps = re.captures(line.as_str())?;
+
+    let binding = caps.get(1).unwrap().as_str().to_uppercase();
+    let cmdstr = binding.as_str();
+    let args = caps.get(2).unwrap().as_str();
+    match cmdstr {
+        "PUT" => {
+            let kv = args.split_once(',')?;
+            Some(KVSMessage::Put {
+                key: kv.0.trim().to_string(),
+                value: kv.1.trim().to_string(),
+            })
+        }
+        "GET" => Some(KVSMessage::Get {
+            key: args.trim().to_string(),
+        }),
+        _ => None,
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,57 @@
+use clap::{ArgEnum, Parser};
+use client::run_client;
+use hydroflow::tokio;
+use hydroflow::util::{bind_udp_socket, ipv4_resolve};
+use server::run_server;
+
+mod client;
+mod helpers;
+mod protocol;
+mod server;
+
+#[derive(Clone, ArgEnum, Debug)]
+enum Role {
+    Client,
+    Server,
+}
+#[derive(Clone, ArgEnum, Debug)]
+enum GraphType {
+    Mermaid,
+    Dot,
+    Json,
+}
+
+#[derive(Parser, Debug)]
+struct Opts {
+    #[clap(arg_enum, long)]
+    role: Role,
+    #[clap(long)]
+    addr: String,
+    #[clap(long)]
+    server_addr: Option<String>,
+    #[clap(arg_enum, long)]
+    graph: Option<GraphType>,
+}
+
+#[tokio::main]
+async fn main() {
+    let opts = Opts::parse();
+
+    match opts.role {
+        Role::Client => {
+            let (outbound, inbound) = bind_udp_socket(opts.addr.clone()).await;
+            println!("Client is bound to {}", opts.addr.clone());
+            println!(
+                "Attempting to connect to server at {}",
+                opts.server_addr.clone().unwrap()
+            );
+            let server_addr = ipv4_resolve(opts.server_addr.clone().unwrap());
+            run_client(outbound, inbound, server_addr, opts.graph.clone()).await;
+        }
+        Role::Server => {
+            let (outbound, inbound) = bind_udp_socket(opts.addr.clone()).await;
+            println!("Listening on {}", opts.addr.clone());
+            run_server(outbound, inbound, opts.graph.clone()).await;
+        }
+    }
+}
